27feebde631154f33925cb63715acbc5f9c19fd1
Liz Izatt <liz@berkeley.edu>
2022-06-05 12:49:43 -0700

    Capturing workspace

---
+++ Assets/FollowTargetAtSpeed.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTargetAtSpeed : MonoBehaviour
+{
+    public GameObject myObject;
+    public GameObject targetObject;
+    public float max_s = 5;
+    public float accel = 5;
+    public float trigger_rad;
+
+
+    private Vector3 initialOffset;
+    private Vector3 cur_v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialOffset = targetObject.transform.position - myObject.transform.position;
+        cur_v = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toTarget = targetObject.transform.position - myObject.transform.position - initialOffset;
+
+        float curdist = toTarget.magnitude;
+        float target_s = curdist > trigger_rad? max_s : 0;
+
+        cur_v += accel * toTarget.normalized;
+        if (cur_v.magnitude > target_s) {
+            cur_v = cur_v.normalized * target_s;
+        }
+        myObject.transform.Translate(cur_v * Time.deltaTime);
+    }
+}
---
+++ Assets/derpyjaz/tex/readme (Reinbuu-sama).txt
@@ -0,0 +1,25 @@
+Tda IA Cross Micro One-Piece Bikini
+Edit by Reinbuu-sama
+
+~*Credits*~
+
+- Tda
+- Stylc
+- mqdl
+- Marbles 
+- Jean
+
+~*Rules*~
+
+- Make sure to follow Tda's rules first and foremost. Readme is included.
+- Editing is fine provided it doesn't voilate the rules of other modelers.
+- Reistribution of an edit is fine with permission. Ask me first, I'll 99% guaranteed say yes.
+- Redistributing this model unedited is not allowed.
+- Credit Tda and Reinbuu-sama when you use this model.
+- When redistributing an edit, keep ALL original readmes with the model.
+
+~*Contact*~
+
+- Contact me if there are any questions.
+- https://reinbuu-sama.deviantart.com
+- https://www.youtube.com/channel/UC--ywvoFoqR58XdnvJVIASw
